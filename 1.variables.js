27ad88715a263c403d2b09884d0dbddfd3523a02
muhammadishaqahmad <152203351+muhammadishaqahmad@users.noreply.github.com>
2024-07-25 22:30:23 +0500

    variables

---
+++ 1.variables.js
@@ -0,0 +1,17 @@
+const accountID = 2323;                    // constant
+let accountEmail = "ishaq@google.com"      //variable :- let is used in the scope and access only in that  scope
+var accountPassword = 'Peshaware';         // variable:- var is used in or out of scope and access in the every scope 
+                                           //....Now a das var is not commonly used b\c of block scope and functional scope....
+
+accountCity = "Peshaware"   // possiablity
+
+// accountID = 2
+// console.log(accountID);   // not allowed to used
+
+accountEmail = "hi@gmail.com"
+accountPassword = "4546"
+accountCity = "Nowshera"
+
+let accountState;    // if we declared a variavle and can't initalize it . the output of that variable is und
+
+console.table([accountEmail,accountPassword,accountCity,accountState])
