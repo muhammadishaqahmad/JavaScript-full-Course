4baaa60acdbe2d7371abf8f850a8863b8c26541d
muhammadishaqahmad <152203351+muhammadishaqahmad@users.noreply.github.com>
2024-07-25 22:51:34 +0500

    DataTypes

---
+++ 2.DataTypes.js
@@ -0,0 +1,30 @@
+"use strict "; //treat all code as new code
+ 
+//Premative Data Type
+
+     // Number
+     // String
+     // Boolean
+     // BigInt
+     // null
+     // undefined
+     //   Symbol
+     
+// Non Premative Data Type
+     // object
+ 
+let name = "Ishaq"
+let age = 22
+let isloggedIn = true
+
+console.log(name);
+console.log(age);
+console.log(isloggedIn);
+
+let loggedOut = null
+console.log(loggedOut); // output:- null
+
+console.log(typeof name);               //string;
+console.log(typeof age);                //number;
+console.log(typeof isloggedIn);         //boolean;
+console.log(typeof loggedOut);          //......null typeOf is object.......
