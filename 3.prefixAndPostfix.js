04de2e21dfc28c9904f3695d4b0e8b1af00a96d6
muhammadishaqahmad <152203351+muhammadishaqahmad@users.noreply.github.com>
2024-07-26 22:46:20 +0500

    prefix and postfix

---
+++ 3.prefixAndPostfix.js
@@ -0,0 +1,12 @@
+// Prefix 
+let x, y;
+x = 5;
+y = ++x;   // first the x value incremented then assinged to y 
+console.log(y);
+console.log(x);
+
+// Postfix
+let a, b = 5;
+a = b++;
+console.log(a); // first the b value  assinged to  a , then the b value increment
+console.log(b); 
