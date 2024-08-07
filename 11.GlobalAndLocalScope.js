ae4d1d0082184a0ea0e9312be4f56df698a56c1b
muhammadishaqahmad <152203351+muhammadishaqahmad@users.noreply.github.com>
2024-08-07 23:15:32 +0500

    scope

---
+++ 11.GlobalAndLocalScope.js
@@ -0,0 +1,21 @@
+// let a = 10
+// const b = 1200
+// var c = 90
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+if (true) {
+    let a = 10;
+    const b = 1200;
+    var c = 90;
+}
+//console.log(a); // a is not defined
+//console.log(b); // b is not defined
+console.log(c);
+
+// let and cosnt are block scope element
+// var is global scope element
+
+// inspect global scope is differnt and in code the global is different 
