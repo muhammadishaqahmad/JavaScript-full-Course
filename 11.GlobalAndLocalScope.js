0b4dfb0df34f6db7f53392c3f21df9b0ce6df4ea
muhammadishaqahmad <152203351+muhammadishaqahmad@users.noreply.github.com>
2024-08-08 22:26:46 +0500

    all about scope

--- 11.GlobalAndLocalScope.js
+++ 11.GlobalAndLocalScope.js
@@ -13,9 +13,44 @@ if (true) {
 }
 //console.log(a); // a is not defined
 //console.log(b); // b is not defined
-console.log(c);
+//console.log(c);
 
 // let and cosnt are block scope element
 // var is global scope element
 
-// inspect global scope is differnt and in code the global is different 
+// inspect global scope is differnt and in code the global is different
+
+// *********************************************************************************************************
+
+function one () {                     // this method is also called clousere in js
+  const userName = 'ishaq';
+  function two() {
+    const website = 'youtube';
+    console.log(userName);   // function one is in the scope
+  }
+  // console.log(website); //website is not defined .and its out of scope
+  two();
+}
+//one();
+
+// if (true) {
+//     const userName = "ishaq"
+//     if (userName === "ishaq") {
+//         const website = "youtube"
+//         console.log(userName + "," + website);
+//     }
+//     //console.log(website); // error its out of scope
+// }
+//console.log(userName);// error its out of scope
+
+console.log(addOne(3));
+function addOne(num1) {
+    return num1 + 1;
+}
+
+
+//console.log(addTwo(4)); // Cannot access 'addTwo' before initialization , only use after function
+const addTwo = function(num1){ // the addtwo variable hold the value
+    return num1 + 2;
+}
+console.log(addTwo(4));
